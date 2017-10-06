8c766b8639097c52d5fdaab3c1816b3c1fc526dc
Oliver <oliver.glant@gmail.com>
2017-10-06 11:56:10 +0200

    Initial commit

---
+++ src/main/java/ID1020/Lab5/Paths.java
@@ -0,0 +1,11 @@
+package ID1020.Lab5;
+
+import se.kth.id1020.Graph;
+import se.kth.id1020.DataSource;
+
+public class Paths {
+ public static void main(String[] args) {
+ Graph g = DataSource.load();
+ // work on g
+ }
+}
